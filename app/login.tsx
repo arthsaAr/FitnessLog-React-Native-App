cc97d902876ee97670e87e8f3c337dc8e25f50c5
arthsaAr <kcraashtra431qwe@gmail.com>
2025-12-22 21:43:02 -0700

    Starting login screen setup

---
+++ app/login.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function loginScreen() {
+    return (
+        <View className="flex-1 bg-primary justify-center">
+              <Text className="text-5xl text-blue-500 font-bold">Login</Text>
+        </View>
+    );
+}
